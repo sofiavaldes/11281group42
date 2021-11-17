e58ed394932b26c13f3f1e1a815cf905f0d3a362
Graham Vaith <grahamvaith@ufl.edu>
2021-11-17 16:10:30 -0500

    refactoring the organization of the code base

--- dashboard.js
+++ dashboard.js
@@ -1,53 +1,11 @@
 /* globals Chart:false, feather:false */
+function myFunction(id) {
 
-(function () {
-  'use strict'
+  document.getElementById(id).innerText = ppFunction(id);
+  var popup = document.getElementById(id);
+  popup.classList.toggle("show");
+}
 
-  feather.replace({ 'aria-hidden': 'true' })
-
-  // Graphs
-  var ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })
-})()
+function ppFunction(county){
+  return county;
+}
